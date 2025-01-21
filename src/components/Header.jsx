d38bbc1448b40f7e39cf1968287a876f19e97da8
coder4al <coder4al@gmail.com>
2025-01-21 15:57:17 +0530

    image bug fixxing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 // Header.js
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logo from "../assets/MedVita_Logo.svg"
 import "./Header.css"; // Import the CSS file for styling
 
 const Header = () => {
     return (
         <header className="header">
             <div className="logo ">
-                <h1> <img src="\src\assets\MedVita_Logo.svg" width={50} height={50} alt="" />
+                <h1> <img src={logo} width={50} height={50} alt="" />
                     MedVita</h1>
             </div>
             <nav className="nav">
